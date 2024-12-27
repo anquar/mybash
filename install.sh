132913ac577fd2443824f0243d4cc46e896afeb1
anqr <anqrwu@gmail.com>
2024-12-27 23:57:50 +0800

    重构脚本逻辑

--- install.sh
+++ install.sh
@@ -3,153 +3,82 @@
 source ./support/base.sh
 source ./support/func.sh
 
-RELEASE=$(check_os)
-PKG_MANAGER=$(check_package_manager)
-NETSTATUS=$(check_network)
+# 初始化环境
+init_environment() {
+    RELEASE=$(check_os)
+    PKG_MANAGER=$(check_package_manager)
+    NETSTATUS=$(check_network)
+    CURRENT_SHELL=$(basename "$SHELL")
 
-# 根据网络情况选择执行模式
-if [[ $NETSTATUS -eq 0 ]]; then
-    if ask_user "无任何网络连接，是否继续执行?"; then
-        echo "好的，我们继续执行"
-    else
-        LOGI "现在退出" && exit
-    fi
-    MODE=0
-elif [[ $NETSTATUS -gt 0 && $NETSTATUS -le 3 ]]; then
-    MODE=1
-elif [[ $NETSTATUS -gt 4 && $NETSTATUS -le 7 ]]; then
-    MODE=2
-else
-    LOGE "未知的网络状态，现在退出" && exit 1
-fi
-
-# 设置安装命令，静默安装
-PKG_INSTALL_CMD=''
-if [[ x"${PKG_MANAGER}" == x"yum" ]]; then
-    PKG_INSTALL_CMD="yum install -q -y"
-elif [[ x"${PKG_MANAGER}" == x"apt" ]]; then
-    PKG_INSTALL_CMD="apt install -qq -y"
-else
-    LOGE "无法确定包管理器" && exit 1
-fi
-if [[ $(id -u) -ne 0 ]]; then
-    PKG_INSTALL_CMD="sudo ${PKG_INSTALL_CMD}"
-fi
-
-LOGI "安装常用软件包..."
-case "$RELEASE" in
-    "rhel" | "centos" | "anolis")
-        $PKG_INSTALL_CMD curl tar vim fd-find ripgrep util-linux-user
-        ;;
-    "fedora")
-        $PKG_INSTALL_CMD tar vim util-linux-user
-        ;;
-    "debian")
-        $PKG_INSTALL_CMD curl tar vim fd-find ripgrep passwd
-        ;;
-    *)
-        LOGW "未知的操作系统($RELEASE)，跳过软件安装"
-        ;;
-esac
-
-# 检查zsh是否已安装
-if ! command -v zsh &> /dev/null; then
-    LOGI "zsh解释器未安装，开始安装..."
-    $PKG_INSTALL_CMD zsh
-fi
-# 检查zsh是否是默认解释器
-if [ $(basename "$SHELL") != "zsh" ]; then
-    LOGI "zsh不是默认的解释器，开始设置为默认..."
-    if [[ $(id -u) -eq 0 ]]; then
-	chsh -s $(which zsh)
-    else
-        sudo chsh -s $(which zsh)
-    fi
-fi
-
-# 非本地模式才进一步配置
-if [[ $MODE -ne 0 ]]; then
-    if mv -n ~/.zshrc ~/.zshrc-backup-$(date +"%Y-%m-%d"); then
-        LOGI "已将当前 .zshrc 备份到 .zshrc-backup-date"
-    fi
-
-    ZSH_CONFIG_PATH="$HOME/.config/zsh"
-    mkdir -p ${ZSH_CONFIG_PATH}
-    mkdir -p ${ZSH_CONFIG_PATH}/zshrc
-    mkdir -p ~/.cache/zsh/
-    LOGI "zsh配置路径为${ZSH_CONFIG_PATH}"
-    cp -f .zshrc ~/
-    cp -f config.zsh ${ZSH_CONFIG_PATH}/
-
-    LOGI "开始安装 oh-my-zsh ..."
-    if [ -d ${ZSH_CONFIG_PATH}/oh-my-zsh ]; then
-        cd ${ZSH_CONFIG_PATH}/oh-my-zsh && git pull
-    else
-       if [[ $MODE -eq 1 ]]; then
-           git clone --depth=1 https://gitee.com/mirrors/oh-my-zsh.git ${ZSH_CONFIG_PATH}/oh-my-zsh
-       else
-           git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ${ZSH_CONFIG_PATH}/oh-my-zsh
-       fi
-    fi
-
-    plugins=("zsh-autosuggestions" "zsh-syntax-highlighting" "zsh-completions" "zsh-history-substring-search")
-
-    for plugin in "${plugins[@]}"; do
-        LOGI "开始安装 oh-my-zsh 插件 ${plugin} ..."
-        if [ -d ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/plugins/${plugin} ]; then
-            cd ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/plugins/${plugin} && git pull
+    # 设置网络模式
+    if [[ $NETSTATUS -eq 0 ]]; then
+        if ask_user "无任何网络连接，是否继续执行?"; then
+            echo "好的，我们继续执行"
         else
-            if [[ $MODE -eq 1 ]]; then
-                git clone --depth=1 https://gitclone.com/github.com/zsh-users/${plugin}.git ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/plugins/${plugin}
-            else
-                git clone --depth=1 https://github.com/zsh-users/${plugin}.git ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/plugins/${plugin}
-            fi
+            LOGI "现在退出" && exit
         fi
-    done
-
-    LOGI "开始安装主题..."
-    if [ -d ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/themes/powerlevel10k ]; then
-        cd ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/themes/powerlevel10k && git pull
+        MODE=0
+    elif [[ $NETSTATUS -gt 0 && $NETSTATUS -le 3 ]]; then
+        MODE=1
+    elif [[ $NETSTATUS -gt 4 && $NETSTATUS -le 7 ]]; then
+        MODE=2
     else
-        if [[ $MODE -eq 1 ]]; then
-            git clone --depth=1 https://gitclone.com/github.com/romkatv/powerlevel10k.git ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/themes/powerlevel10k
-        else
-            git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/themes/powerlevel10k
-        fi
+        LOGE "未知的网络状态，现在退出" && exit 1
     fi
 
-    if [[ $MODE -eq 2 ]]; then
-        LOGI "开始安装插件 fzf ..."
-        if [ -d ${ZSH_CONFIG_PATH}/fzf ]; then
-            cd ${ZSH_CONFIG_PATH}/fzf && git pull
-            ${ZSH_CONFIG_PATH}/fzf/install --all --key-bindings --completion --no-update-rc
-        else
-            if [[ $MODE -eq 1 ]]; then
-                git clone --depth 1 https://gitclone.com/github.com/junegunn/fzf.git ${ZSH_CONFIG_PATH}/fzf
-            else
-                git clone --depth 1 https://github.com/junegunn/fzf.git ${ZSH_CONFIG_PATH}/fzf
-            fi
-            ${ZSH_CONFIG_PATH}/fzf/install --all --key-bindings --completion --no-update-rc
-        fi
-    fi
-
-    LOGI "开始安装插件 k ..."
-    if [ -d ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/plugins/k ]; then
-        cd ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/plugins/k && git pull
+    # 设置包管理器
+    if [[ x"${PKG_MANAGER}" == x"yum" ]]; then
+        PKG_INSTALL_CMD="yum install -q -y"
+    elif [[ x"${PKG_MANAGER}" == x"apt" ]]; then
+        PKG_INSTALL_CMD="apt install -qq -y"
     else
-        if [[ $MODE -eq 1 ]]; then
-            git clone --depth 1 https://gitclone.com/github.com/supercrabtree/k ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/plugins/k
-        else
-            git clone --depth 1 https://github.com/supercrabtree/k ${ZSH_CONFIG_PATH}/oh-my-zsh/custom/plugins/k
-        fi
+        LOGE "无法确定包管理器" && exit 1
     fi
+    [[ $(id -u) -ne 0 ]] && PKG_INSTALL_CMD="sudo ${PKG_INSTALL_CMD}"
 
-fi
+    export RELEASE PKG_MANAGER MODE PKG_INSTALL_CMD CURRENT_SHELL
+}
 
-#LOGI "开始设置时区..."
-timedatectl set-timezone Asia/Shanghai
+# 安装基础软件包
+install_base_packages() {
+    LOGI "安装常用软件包..."
+    case "$RELEASE" in
+        "rhel" | "centos" | "anolis")
+            $PKG_INSTALL_CMD curl tar vim fd-find ripgrep util-linux-user
+            ;;
+        "fedora")
+            $PKG_INSTALL_CMD tar vim util-linux-user
+            ;;
+        "debian")
+            $PKG_INSTALL_CMD curl tar vim fd-find ripgrep passwd
+            ;;
+        *)
+            LOGW "未知的操作系统($RELEASE)，跳过软件安装"
+            ;;
+    esac
+}
 
-LOGI "开始设置git..."
-setup_git
-LOGI "开始设置vim..."
-setup_vim
+# 主函数
+main() {
+    init_environment
+    install_base_packages
+
+    case "$CURRENT_SHELL" in
+        "bash")
+            setup_bash
+            ;;
+        "zsh")
+            setup_zsh
+            ;;
+        *)
+            LOGW "当前shell($CURRENT_SHELL)不支持，跳过配置"
+            ;;
+    esac
+
+    # 设置时区和其他通用配置
+    timedatectl set-timezone Asia/Shanghai
+    setup_git
+    setup_vim
+}
+
+main
--- support/func.sh
+++ support/func.sh
@@ -111,7 +111,7 @@ function setup_git() {
     git config --global core.editor "vim"
     git config --global pull.rebase true
     git config --global user.email "wuaqcn@qq.com"
-    git config --global user.name "muyz"
+    git config --global user.name "wuaq"
 }
 
 # 设置vim
@@ -127,4 +127,214 @@ function setup_vim() {
         git clone git@gitee.com:wuaq/vimcan.git ~/.vim
     fi
     sh ~/.vim/install.sh
+}
+
+# 通用的 git clone 函数
+git_clone_or_pull() {
+    local repo_url="$1"
+    local target_dir="$2"
+    local proxy_url="$3"
+
+    if [ -d "$target_dir" ]; then
+        cd "$target_dir" && git pull
+        return
+    fi
+
+    if [[ $MODE -eq 1 && -n "$proxy_url" ]]; then
+        git clone --depth=1 "$proxy_url" "$target_dir"
+    else
+        git clone --depth=1 "$repo_url" "$target_dir"
+    fi
+}
+
+# 安装 shell 插件
+install_shell_plugin() {
+    local plugin_name="$1"
+    local repo_url="$2"
+    local target_dir="$3"
+    local proxy_url="${4:-${repo_url/github.com/gitclone.com\/github.com}}"
+
+    LOGI "开始安装插件 ${plugin_name} ..."
+    git_clone_or_pull "$repo_url" "$target_dir" "$proxy_url"
+}
+
+# 配置 bash
+setup_bash() {
+    LOGI "当前默认shell是bash，开始配置..."
+
+    # 配置 bash_profile
+    setup_bash_profile
+
+    # 安装 oh-my-bash
+    install_oh_my_bash
+
+    # 非本地模式才安装插件
+    [[ $MODE -ne 0 ]] && install_bash_plugins
+}
+
+# 配置 bash_profile
+setup_bash_profile() {
+    LOGI "检查 ~/.bash_profile 配置..."
+    if [ -f ~/.bash_profile ]; then
+        if ! grep -q "source ~/.bashrc" ~/.bash_profile; then
+            echo -e "\nif [[ -f ~/.bashrc ]]; then\n  source ~/.bashrc\nfi" >> ~/.bash_profile
+            LOGI "已添加 source ~/.bashrc 到 ~/.bash_profile"
+        fi
+    else
+        echo -e "if [[ -f ~/.bashrc ]]; then\n  source ~/.bashrc\nfi" > ~/.bash_profile
+        LOGI "已创建 ~/.bash_profile 并添加必要配置"
+    fi
+}
+
+# 安装 oh-my-bash
+install_oh_my_bash() {
+    if [ -d ~/.oh-my-bash ]; then
+        cd ~/.oh-my-bash && git pull
+    else
+        if [[ $MODE -eq 1 ]]; then
+            bash -c "$(curl -fsSL https://gitclone.com/github.com/ohmybash/oh-my-bash/raw/master/tools/install.sh)"
+        else
+            bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+        fi
+    fi
+}
+
+# 安装 bash 插件
+install_bash_plugins() {
+    LOGI "开始安装 oh-my-bash 插件..."
+    mkdir -p ~/.oh-my-bash/custom/plugins
+
+    # 安装自动补全
+    install_shell_plugin "bash-autosuggestions" \
+        "https://github.com/akinomyoga/ble.sh.git" \
+        ~/.oh-my-bash/custom/plugins/bash-autosuggestions
+
+    # 安装语法高亮
+    install_shell_plugin "bash-syntax-highlighting" \
+        "https://github.com/zdharma-continuum/fast-syntax-highlighting.git" \
+        ~/.oh-my-bash/custom/plugins/bash-syntax-highlighting
+
+    # 安装 fzf（仅完整模式）
+    if [[ $MODE -eq 2 ]]; then
+        install_shell_plugin "fzf" \
+            "https://github.com/junegunn/fzf.git" \
+            ~/.oh-my-bash/custom/plugins/fzf
+        ~/.oh-my-bash/custom/plugins/fzf/install --all --key-bindings --completion --no-update-rc
+    fi
+
+    # 配置 bashrc
+    setup_bashrc
+}
+
+# 配置 bashrc
+setup_bashrc() {
+    # 备份原有配置
+    if [ -f ~/.bashrc ]; then
+        if mv -n ~/.bashrc ~/.bashrc-backup-$(date +"%Y-%m-%d"); then
+            LOGI "已将当前 .bashrc 备份到 .bashrc-backup-date"
+        fi
+    fi
+
+    # 创建新配置
+    cat > ~/.bashrc << 'EOF'
+# Path to your oh-my-bash installation.
+export OSH=~/.oh-my-bash
+
+# 设置主题
+OSH_THEME="powerline"
+
+# 启用插件
+plugins=(
+    core
+    git
+    bash-autosuggestions
+    bash-syntax-highlighting
+)
+
+if [ -f ~/.oh-my-bash/custom/plugins/fzf/fzf.bash ]; then
+    source ~/.oh-my-bash/custom/plugins/fzf/fzf.bash
+fi
+
+source "$OSH/oh-my-bash.sh"
+
+# 用户自定义别名和函数
+alias ll='ls -la'
+alias la='ls -A'
+alias l='ls -CF'
+EOF
+}
+
+# 配置 zsh
+setup_zsh() {
+    [[ $MODE -eq 0 ]] && return
+    LOGI "当前默认shell是zsh，开始配置..."
+
+    # 设置配置目录
+    ZSH_CONFIG_PATH="$HOME/.config/zsh"
+    mkdir -p ${ZSH_CONFIG_PATH}/{zshrc,cache}
+
+    # 备份原有配置
+    if mv -n ~/.zshrc ~/.zshrc-backup-$(date +"%Y-%m-%d"); then
+        LOGI "已将当前 .zshrc 备份到 .zshrc-backup-date"
+    fi
+
+    # 复制配置文件
+    cp -f .zshrc ~/
+    cp -f config.zsh ${ZSH_CONFIG_PATH}/
+
+    # 安装 oh-my-zsh
+    install_oh_my_zsh
+    install_zsh_plugins
+    install_zsh_theme
+}
+
+# 安装 oh-my-zsh
+install_oh_my_zsh() {
+    LOGI "开始安装 oh-my-zsh ..."
+    if [ -d ${ZSH_CONFIG_PATH}/oh-my-zsh ]; then
+        cd ${ZSH_CONFIG_PATH}/oh-my-zsh && git pull
+    else
+        if [[ $MODE -eq 1 ]]; then
+            git clone --depth=1 https://gitee.com/mirrors/oh-my-zsh.git ${ZSH_CONFIG_PATH}/oh-my-zsh
+        else
+            git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git ${ZSH_CONFIG_PATH}/oh-my-zsh
+        fi
+    fi
+}
+
+# 安装 zsh 插件
+install_zsh_plugins() {
+    local plugins=(
+        "zsh-autosuggestions"
+        "zsh-syntax-highlighting"
+        "zsh-completions"
+        "zsh-history-substring-search"
+    )
+
+    for plugin in "${plugins[@]}"; do
+        install_shell_plugin "$plugin" \
+            "https://github.com/zsh-users/${plugin}.git" \
+            "${ZSH_CONFIG_PATH}/oh-my-zsh/custom/plugins/${plugin}"
+    done
+
+    # 安装 k 插件
+    install_shell_plugin "k" \
+        "https://github.com/supercrabtree/k" \
+        "${ZSH_CONFIG_PATH}/oh-my-zsh/custom/plugins/k"
+
+    # 安装 fzf（仅完整模式）
+    if [[ $MODE -eq 2 ]]; then
+        install_shell_plugin "fzf" \
+            "https://github.com/junegunn/fzf.git" \
+            "${ZSH_CONFIG_PATH}/fzf"
+        ${ZSH_CONFIG_PATH}/fzf/install --all --key-bindings --completion --no-update-rc
+    fi
+}
+
+# 安装 zsh 主题
+install_zsh_theme() {
+    LOGI "开始安装主题..."
+    install_shell_plugin "powerlevel10k" \
+        "https://github.com/romkatv/powerlevel10k.git" \
+        "${ZSH_CONFIG_PATH}/oh-my-zsh/custom/themes/powerlevel10k"
 }
