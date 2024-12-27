93ccf972c2be610a03cbe0f28d563831b0a287a9
anqr <anqrwu@gmail.com>
2024-12-28 00:01:04 +0800

    把bash的配置文件提取到单独的文件中

--- support/func.sh
+++ support/func.sh
@@ -235,33 +235,9 @@ setup_bashrc() {
         fi
     fi
 
-    # 创建新配置
-    cat > ~/.bashrc << 'EOF'
-# Path to your oh-my-bash installation.
-export OSH=~/.oh-my-bash
-
-# 设置主题
-OSH_THEME="powerline"
-
-# 启用插件
-plugins=(
-    core
-    git
-    bash-autosuggestions
-    bash-syntax-highlighting
-)
-
-if [ -f ~/.oh-my-bash/custom/plugins/fzf/fzf.bash ]; then
-    source ~/.oh-my-bash/custom/plugins/fzf/fzf.bash
-fi
-
-source "$OSH/oh-my-bash.sh"
-
-# 用户自定义别名和函数
-alias ll='ls -la'
-alias la='ls -A'
-alias l='ls -CF'
-EOF
+    # 复制配置文件
+    LOGI "复制 bash 配置文件..."
+    cp -f config.bash ~/.bashrc
 }
 
 # 配置 zsh
